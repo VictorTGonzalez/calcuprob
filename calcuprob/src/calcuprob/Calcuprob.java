e02df3413498e56390758f17dc315f5e94ec8bf2
VictorTGonzalez <vick4927@gmail.com>
2022-09-20 11:58:21 -0600

    primer cambio

---
+++ calcuprob/src/calcuprob/Calcuprob.java
@@ -0,0 +1,17 @@
+
+package calcuprob;
+
+/**
+ *
+ * @author vick4
+ */
+public class Calcuprob {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
